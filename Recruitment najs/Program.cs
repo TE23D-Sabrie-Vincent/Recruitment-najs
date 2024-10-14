9fdb6dd0d0f64abbf988067f780a71f142e97bd9
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-10-14 09:30:12 +0200

    Notes från lektionen

---
+++ Recruitment najs/Program.cs
@@ -0,0 +1,38 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// // 
+
+// List<string> names = ["Vinny", "Vincent", "Theodor (usch)"];
+// names[1] = "Teddy";
+
+// names.Add("Ludvig");
+// //använd Add i recruitmen
+// names.Add("Simon");
+// names.Add("Kevin");
+
+// names.Remove("Theodor (usch)");
+// names.RemoveAt(1);
+// if (names.Contains("Ludvig") == true)
+// {
+//     System.Console.WriteLine("Yes, det finns i listan");
+// }
+
+// System.Console.WriteLine(string.Join("\n", names));
+
+
+// // for (int i=0; i < names.Count; i++)
+// // {
+// // System.Console.WriteLine(names[i]);
+// // }
+
+
+
+
+// // string name = Console.ReadLine();
+// // if (name == names[1])
+// // {
+// //     System.Console.WriteLine("Hej dumass");
+// // }
+// // System.Console.WriteLine(names[1]);
+
+Console.ReadLine();
+
